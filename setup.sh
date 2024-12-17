48be3bf5ea90c85e8f38057f6274c2518c1d2691
joseporcar <joseaporcar@gmail.com>
2024-12-16 23:07:37 -0400

    Addede some programs

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S fastfetch kitty fish rofi hyprland waybar hyprpaper tlp ttf-jetbrains-mono-nerd nwg-look swayncxdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland;
+sudo pacman -S fastfetch kitty fish rofi hyprland waybar hyprpaper tlp ttf-jetbrains-mono-nerd nwg-look swayncxdg-desktop-portal-hyprland hyprpolkitagent qt5-wayland qt6-wayland brightnessctl rustup cheese;
 
 systemctl --user enable --now hyprpolkitagent.service
 
-yay -S zapzap hyprshot visual-studio-code-bin;
+yay -S zapzap hyprshot hyprsunset visual-studio-code-bin;
 
 sudo sudo systemctl enable tlp.service;
 sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket;
@@ -19,6 +19,8 @@ mkdir -p ~/.local/share/rofi/themes/
 cp rofi/themes/* ~/.local/share/rofi/themes
 mkdir ~/.config/rofi
 cp rofi/config.rasi ~/.config/rofi
+
+rustup default stable
 
 flatpak install io.github.zen_browser.zen vesktop;
 chsh -s /usr/bin/fish
