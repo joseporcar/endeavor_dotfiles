bb2cf05115fa810cf70b02e4191fbd8d8a59cd72
joseporcar <joseaporcar@gmail.com>
2025-03-17 19:21:37 -0400

    updated setup hopefully should work

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-sudo pacman -S fastfetch kitty fish tlp nwg-look qt5-wayland qt6-wayland rustup cheese;
+sudo pacman -S fastfetch kitty fish tlp nwg-look qt5-wayland qt6-wayland rustup cheese &&
 
-yay -S zapzap visual-studio-code-bin gtk-engine-murrine lxappearance colloid-catppuccin-gtk-theme-git helix-git otf-ibm-plex;
+yay -S zapzap visual-studio-code-bin gtk-engine-murrine lxappearance colloid-catppuccin-gtk-theme-git helix-git otf-ibm-plex &&
 
 # GTK theme
-cp -r /usr/share/themes/Colloid-Dark-Compact-Catppuccin/ .config/gtk-4.0/
+cp -r /usr/share/themes/Colloid-Dark-Compact-Catppuccin/ .config/gtk-4.0/ &&
 
-sudo chmod a+x ./hypr/setup.sh
+sudo chmod a+x ./hypr/setup.sh &&
 
 # TLP
-sudo sudo systemctl enable tlp.service;
-sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket;
-cp tlp.conf /etc;
+sudo sudo systemctl enable tlp.service &&
+sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket &&
+sudo cp tlp.conf /etc &&
 
-cp terminal/kitty.conf ~/.config/kitty/kitty.conf
-cp terminal/fish.conf 
+cp terminal/kitty.conf ~/.config/kitty/kitty.conf &&
+cp terminal/fish.conf &&
 
-rustup default stable
+rustup default stable &&
 
-flatpak install io.github.zen_browser.zen vesktop;
+flatpak install io.github.zen_browser.zen vesktop &&
 chsh -s /usr/bin/fish
 
 
