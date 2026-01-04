4e63247a9869c38f58648e8e9d823217e5b35082
joseporcar <joseaporcar@gmail.com>
2026-01-04 13:01:42 +0100

    wallpaper works now

--- hypr/startup.sh
+++ hypr/startup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+hyprpaper&
 sleep 5; # Mostly necesary for laptop to connect to wifi and finish any boot stuff
 zapzap &
 
 # This was a solution for when I had the issue of mic not working on boot. Uncomment if needed.
 # wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 1;
 # wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 0;
-hyprpaper&
+
 # TODO: this doesnt work
 hyprctl setcursor "Catppuccin Mocha Green" 24 &
 gsettings set org.gnome.desktop.interface cursor-theme catppuccin-mocha-green-cursors &
